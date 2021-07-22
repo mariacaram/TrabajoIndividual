92ac5b2e72e04bccf0e4eea8a40e3f7bf7284304
Maria <maria.caram@gmail.com>
2021-07-22 17:09:05 -0300

    Avance 1

---
+++ desafio.js
@@ -0,0 +1,40 @@
+const categoriaInput = document.getElementById('categoria');
+const descripcionInput = document.getElementById('descripcion');
+const =contenidoTabla = document.getElementById ('contenidoTabla')
+const notas = [];
+
+function AgregarNota(event) {
+    event.preventDefault();
+    const categoria = categoriaInput.value;
+    const descripcion = descripcionInput.value;
+
+    const nuevaNota = {
+        categoria: categoria,
+        descripcion: descripcion
+    };
+
+    notas.push(nuevaNota);
+    event.target.reset();
+    mostrarNotas()
+}
+
+
+
+function mostrarNotas() {
+
+    const contenido = notas.map(function (nota){
+     
+    const tr = `
+    
+    <tr>
+    <th scope="row">1</th>
+    <td>Mark</td>
+    <td><button class="btn btn-warning">Editar</button></td>
+
+    </tr>
+    
+    `;
+    return tr
+});
+
+contenidoTabla
